4b89c61a0e336c73d8a3b29ca61d9477eba9d4be
robmikh <rob.mikh@outlook.com>
2024-07-04 00:13:00 -0700

    threshold shader

---
+++ shaders/src/ThresholdShader.hlsl
@@ -0,0 +1,54 @@
+cbuffer Parameters : register(b0)
+{
+    float ThresholdValue;
+};
+
+Texture2D<unorm float4> inputTexture : register(t0);
+
+RWTexture2D<unorm float4> outputTexture : register(u0);
+
+float ApplyThreshold(float value)
+{
+    if (value >= ThresholdValue)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+[numthreads(16, 16, 1)]
+void main( uint3 DTid : SV_DispatchThreadID )
+{
+    uint2 position = DTid.xy;
+
+    uint width;
+    uint height;
+    inputTexture.GetDimensions(width, height);
+
+    if (position.x < width && position.y < height)
+    {
+        float4 pixel = inputTexture[position];
+
+        //pixel.x = ApplyThreshold(pixel.x);
+        //pixel.y = ApplyThreshold(pixel.y);
+        //pixel.z = ApplyThreshold(pixel.z);
+
+        float4x4 grayscaleMatrix = 
+        {
+            0.299f, 0.299f, 0.299f, 0.0f,
+            0.587f, 0.587f, 0.587f, 0.0f,
+            0.114f, 0.114f, 0.114f, 0.0f,
+              0.0f,   0.0f,   0.0f, 1.0f,
+        };
+
+        float4 grayscalePixel = mul(pixel, grayscaleMatrix);
+        float thresholdedValue = ApplyThreshold(grayscalePixel.x);
+
+        pixel.x = thresholdedValue;
+        pixel.y = thresholdedValue;
+        pixel.z = thresholdedValue;
+        pixel.w = 1.0f;
+
+        outputTexture[position] = pixel;
+    }
+}
